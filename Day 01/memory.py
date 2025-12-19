43e51156eb5d206bc22a0fe388ea6e172426d20d
arafatshuvo990 <arafatshuvoworks@outlook.com>
2025-12-19 18:06:28 +0600

    initial for the next couple of days

---
+++ Day 01/memory.py
@@ -0,0 +1,10 @@
+sugar_cane1 = 12;
+print(f"initial sugar_cane = {sugar_cane1}");
+
+##memory location
+print(f"memory location of sugar_cane for initial = {id(sugar_cane1)}")
+sugar_cane2 = 22;
+print(f"then sugar_cane = {sugar_cane2}")
+
+##memory location
+print(f"memory location of sugar_cane for then = {id(sugar_cane2)}")
