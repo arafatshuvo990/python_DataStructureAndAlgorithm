7117f7f3f488cfb59ad49c6c2bd1ba4c652bd677
arafatshuvo990 <arafatshuvoworks@outlook.com>
2025-12-20 13:24:01 +0600

    new solution added

--- Day 02/thermostat.py
+++ Day 02/thermostat.py
@@ -1,9 +1,21 @@
-device_status = input(("What is the current status: ")).lower()
-device_temp = input(("what is the current temp: "))
+# device_status = input(("What is the current status: ")).lower()
+# device_temp = input(("what is the current temp: "))
 
-if device_status == "active" and device_temp >= 35:
-    print(f"WARN: hign temparature alert")
-elif device_status != "active":
-    print(f"device is offline")
+# if device_status == "active" and device_temp >= 35:
+#     print(f"WARN: hign temparature alert")
+# elif device_status != "active":
+#     print(f"device is offline")
+# else:
+#     print(f"Device temp is normal")
+
+device_status = "active";
+device_temp = 36;
+
+if device_status == 'active':
+    if device_temp > 35:
+        print(f"WARN: hign temparature alert")
+    else:
+        print(f"Device temp is normal")
 else:
-    print(f"Device temp is normal")
+    print(f"device is offline")
+    
